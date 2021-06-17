3e774c79fab0f39e1ed8b18fbf5eb1544e2218e3
mnerisilva <marceloneri@gmail.com>
2021-06-17 13:59:36 -0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 window.addEventListener("DOMContentLoaded", function(){
     console.log('carrgou');
-    let mask = document.getElementById("mask");
+    //let mask = document.getElementById("mask");
     let botao_plataforma_ativa = document.getElementById("botao-plataforma-ativa");
     let plataforma1 = document.getElementById("plataforma1");
     botao_plataforma_ativa.addEventListener("click", function(){
         console.log("clicou no botão");
-        mask.style.opacity = 1;
+        //mask.style.opacity = 1;
+        mask.classList.add("mostra-mask");
         plataforma1.classList.add("amplia");
     });
     mask.style.opacity = 0;
